d42de81d2ddc85711f76dd9c51b72998f6932081
clancyz <clancyzhu@vip.qq.com>
2016-11-02 11:00:40 +0800

    build 1.0.0

--- dist/components/modal/index.js
+++ dist/components/modal/index.js
@@ -23,6 +23,8 @@
         return __webpack_require__(0);
     }([ function(module, exports, __webpack_require__) {
         module.exports = __webpack_require__(4);
+    }, function(module, exports) {}, function(module, exports) {
+        module.exports = ' <div class="xcui-modal-wrapper xcui-modal-mask" @click=maskClose v-el:modal-mask v-show=show> <div class=xcui-modal tabindex=-1 @keydown.esc=cancel :style=style :class=[sizeClass,className]> <div class=xcui-modal-header v-if=showHeader> <slot name=header> <span class=xcui-modal-title>{{title}}</span> </slot> <slot name=close> <i class="xcui-modal-header-close glyphicon glyphicon-remove" @click=cancel v-if=showCloseButton></i> </slot> </div> <div class=xcui-modal-body :style=contentStyle> <slot>{{content}}</slot> </div> <div class=xcui-modal-footer v-if=showFooter> <slot name=footer> <button type=button name=button v-if=showOkButton @click=ok class="btn xcui-modal-btn btn-primary">{{okText}}</button> <button type=button name=button v-if=showCancelButton @click=cancel class="btn xcui-modal-cancel-btn btn-default">{{cancelText}}</button> </slot> </div> </div> </div> ';
     }, function(module, exports) {
         "use strict";
         Object.defineProperty(exports, "__esModule", {
@@ -142,14 +144,12 @@
                 }
             }
         };
-    }, function(module, exports) {}, function(module, exports) {
-        module.exports = ' <div class="xcui-modal-wrapper xcui-modal-mask" @click=maskClose v-el:modal-mask v-show=show> <div class=xcui-modal tabindex=-1 @keydown.esc=cancel :style=style :class=[sizeClass,className]> <div class=xcui-modal-header v-if=showHeader> <slot name=header> <span class=xcui-modal-title>{{title}}</span> </slot> <slot name=close> <i class="xcui-modal-header-close glyphicon glyphicon-remove" @click=cancel v-if=showCloseButton></i> </slot> </div> <div class=xcui-modal-body :style=contentStyle> <slot>{{content}}</slot> </div> <div class=xcui-modal-footer v-if=showFooter> <slot name=footer> <button type=button name=button v-if=showOkButton @click=ok class="btn xcui-modal-btn btn-primary">{{okText}}</button> <button type=button name=button v-if=showCancelButton @click=cancel class="btn xcui-modal-cancel-btn btn-default">{{cancelText}}</button> </slot> </div> </div> </div> ';
     }, function(module, exports, __webpack_require__) {
         var __vue_script__, __vue_template__;
         var __vue_styles__ = {};
-        __webpack_require__(2);
-        __vue_script__ = __webpack_require__(1);
-        __vue_template__ = __webpack_require__(3);
+        __webpack_require__(1);
+        __vue_script__ = __webpack_require__(3);
+        __vue_template__ = __webpack_require__(2);
         module.exports = __vue_script__ || {};
         if (module.exports.__esModule) module.exports = module.exports.default;
         var __vue_options__ = typeof module.exports === "function" ? module.exports.options || (module.exports.options = {}) : module.exports;
