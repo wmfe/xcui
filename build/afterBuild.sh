028bd8647e18eabeceabb9c7b612276719055b54
lupeipeng <lupeipeng@iwaimai.baidu.com>
2017-01-22 12:01:10 +0800

    顺序修改

--- build/afterBuild.sh
+++ build/afterBuild.sh
@@ -26,14 +26,14 @@ GitAddSite1_0 () {
     git rm -rf ./static
     git rm ./index.html
     cp ../src/index.html ./index.html
+    cp ../build/empty_circle.yml ./circle.yml
+    sed -i '15i\    <script type="text/javascript" src="./static/app.js"></script>' ./index.html
+    mkdir static
     if [ ! -f "../site/app.js"]; then
         exit 1
     else
         cp ../site/app.js ./static/app.js
     fi
-    cp ../build/empty_circle.yml ./circle.yml
-    sed -i '15i\    <script type="text/javascript" src="./static/app.js"></script>' ./index.html
-    mkdir static
     cp -r ../src/assets/* ./static/
     git add ./index.html
     git add ./static
