e383ae4134dfe1860ce14b1fef13ac005adf6544
clancyz <clancyzhu@vip.qq.com>
2017-03-31 16:32:49 +0800

    fix some build config

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,6 +5,7 @@
 var webpack = require('webpack');
 var config = require('./webpack.base.conf');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 // eval-source-map is faster for development
 // config.devtool = '#eval-source-map'
@@ -31,6 +32,21 @@ config.plugins = (config.plugins || []).concat([
     new HtmlWebpackPlugin({
         filename: 'index.html',
         template: 'src/index.html'
+    }),
+    new FaviconsWebpackPlugin({
+        logo: 'src/assets/favicon.png',
+        icons: {
+              android: false,
+              appleIcon: false,
+              appleStartup: false,
+              coast: false,
+              favicons: true,
+              firefox: false,
+              opengraph: false,
+              twitter: false,
+              yandex: false,
+              windows: false
+            }
     })
 ]);
 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 /**
- * @file webpack.npm.base.conf.js
  * entry point
  */
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Helpers from './demos/helpers/install';
-import xcui from './components/index';
+import XCUI from './components/index';
 import App from './App';
 
-Vue.use(xcui);
+Vue.use(XCUI);
 Vue.use(VueRouter);
 Vue.use(Helpers);
-Vue.config.devtools = true;
 new Vue(App).$mount('#app');
