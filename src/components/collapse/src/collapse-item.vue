e5cccddb3b4e857fc820064b3eb715da4c772be2
gengcaili_iwm.baidu.com_waimai <gengcaili@iwaimai.baidu.com>
2017-06-06 20:04:18 +0800

    fix

--- src/components/collapse/src/collapse-item.vue
+++ src/components/collapse/src/collapse-item.vue
@@ -5,8 +5,8 @@
             <x-icon :name="isOpen ? iconUpName : iconDownName" size="14"></x-icon>
             <slot name="titleRight">{{title}}</slot>
         </div>
-        <div class="x-collapse-item-wrap">
-            <div class="x-collapse-item-content" :style="{height: contentHeight, display: isOpen ? '' : 'none'}" ref="content">
+        <div class="x-collapse-item-wrap" :style="{height: contentHeight}" ref="content">
+            <div class="x-collapse-item-content">
                 <slot></slot>
             </div>
         </transition>
