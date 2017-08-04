b0f77779909c1e46708007b143c540f490077940
Chongyu Zhu <clancyzhu@vip.qq.com>
2017-08-04 15:24:32 +0800

    update build due to webpack3 issues (#150)

--- build/dev-server.js
+++ build/dev-server.js
@@ -31,12 +31,13 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   heartbeat: 2000
 })
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
-})
+// temporory remove because webpack3 issue https://github.com/vuejs-templates/webpack/issues/751
+// compiler.plugin('compilation', function (compilation) {
+//   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+//     hotMiddleware.publish({ action: 'reload' })
+//     cb()
+//   })
+// })
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
