693f3af59306735d360758dc823a2713e0f4b690
clancyz <clancyzhu@vip.qq.com>
2017-12-15 16:29:56 +0800

    fix docs

--- demos/components/customTheme.vue
+++ demos/components/customTheme.vue
@@ -5,9 +5,9 @@ xcui支持一定程度上的样式定制，可以满足不同的平台间主题�
 
 ## 样式变量
 
-xcui样式使用了(Less)[http://lesscss.org/]作为开发语言，而且抽象了一系列可定义的全局变量。可以根据具体需求做相关的调整。
+xcui样式使用了[Less](http://lesscss.org/)作为开发语言，而且抽象了一系列可定义的全局变量。可以根据具体需求做相关的调整。
 
-点击这里可以查看(默认样式变量)[https://github.com/wmfe/xcui/blob/2.0/src/less/theme/default.less]的集合。
+点击这里可以查看[默认样式变量](https://github.com/wmfe/xcui/blob/2.0/src/less/theme/default.less)的集合。
 
 ## 定制样式变量 （全局引入）
 
